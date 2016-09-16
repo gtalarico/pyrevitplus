fc21de7fbbfc0176ec5b6a23b4fbf5397a90fb69
Gui Talarico <gtalarico@gmail.com>
2016-09-16 10:49:54 -0400

    Rewrote logic using sorted list, faster more stable

--- pyRevitPlus.tab/MiscTools.panel/CycleType_CycleType.py
+++ pyRevitPlus.tab/MiscTools.panel/CycleType_CycleType.py
@@ -33,50 +33,26 @@ uidoc = __revit__.ActiveUIDocument
 __window__.Close()
 
 
-def dump_types():
-    type_list = []
-    for family_type in family_type_names:
-        type_list.append(family_type.Name)
-
-    with open(temp, 'w') as fp:
-        pickle.dump(type_list, fp)
-    return type_list
-
-
 if not doc.IsFamilyDocument:
     TaskDialog.Show('pyRevitPlus', 'Must be in Family Document.')
 
 else:
     family_types = [x for x in doc.FamilyManager.Types]
+    sorted_type_names = sorted([x.Name for x in family_types])
     current_type = doc.FamilyManager.CurrentType
-    temp = os.path.join(gettempdir(), 'CycleTypes')
-    
-    try:
-        with open(temp, 'r') as fp:
-            type_list = pickle.load(fp)
-    except IOError:
-            type_list = dump_types()
 
-    is_next = False
-    max = 0
-    while True:
-        max += 1
-        if max > 10:
-            raise Exception('Max Limit')
+    # Iterate through sorted list of type names, return name of next in list
+    for n, type_name in enumerate(sorted_type_names):
+        if type_name == current_type.Name:
+            try:
+                next_family_type_name = sorted_type_names[n + 1]
+            except IndexError:
+                # wraps list back to 0 if current is last
+                next_family_type_name = sorted_type_names[0]
 
-        for family_type in family_types:
-            if family_type.Name == current_type.Name:
-                print('Found Current.')
-                is_next = True
-                continue
-            if is_next:
-                t = Transaction(doc, 'Cycle Type')
-                t.Start()
-                doc.FamilyManager.CurrentType = family_type
-                t.Commit()
-                sys.exit()
-
-
-
-
-        #
+    for family_type in family_types:
+        if family_type.Name == next_family_type_name:
+            t = Transaction(doc, 'Cycle Type')
+            t.Start()
+            doc.FamilyManager.CurrentType = family_type
+            t.Commit()
